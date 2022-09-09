955b17e559fdefed66df12c8a5e3ff713d7e0508
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-09-09 13:08:18 -0300

    create json

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-const fetchPokemon = (pokemon) => {
-    const APIResponse = fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+const fetchPokemon = async(pokemon) => {
+    const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    console.log(data);
 }
+fetchPokemon('25');
