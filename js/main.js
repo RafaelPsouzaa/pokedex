b48074a17d390c1a00d5cdbba1134985c6bf11f4
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-09-09 14:19:31 -0300

    creating input

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
+const pokemonForm = document.querySelector('.form');
+
+
 const fetchPokemon = async(pokemon) => {
     const APIResponse = await fetch (`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
     const data = await APIResponse.json();
     console.log(data);
+    return data;
 }
-fetchPokemon('25');
+
+const renderPokemon = async (pokemon) =>{
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];  
+
+}
+renderPokemon('150');
+// 43:03 video
